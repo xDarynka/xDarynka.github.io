1b71a2bab005934376e8e08c2d2fd2a08fe8d975
xDarynka <sokolnykova@gmail.com>
2015-12-18 11:53:01 +0200

    upl

---
+++ JavaScript/js-task-3_Sokolnykova/js/index.js
@@ -0,0 +1,48 @@
+
+// function NotificationManager()  {
+
+// 	 	showNotification: function(text) {
+// 		var div = document.createElement('div');
+// 	  	div.className = "notification";
+// 	  	document.body.appendChild(div);
+// 	 	div.innerHTML = text;
+  		
+//   		}, 
+
+// 		setNotificationTimeout: function() {
+// 	    setTimeout(div.parentNode.removeChild(div));
+// 	 	}
+
+// }
+
+// var nm = new NotificationManager();
+// nm.showNotification("This is first notification");
+
+
+
+
+
+
+function NotificationManager(){
+ this.timeOut =0;
+ return this;
+
+ }
+
+
+NotificationManager.prototype.setNotificationTimeout = function(value){
+ this.timeOut = value;
+}
+
+
+document.addEventListener('DOMContentLoaded', (function(){
+
+var nm = new NotificationManager();
+// console.log(nm.timeOut.toString());
+
+nm.setNotificationTimeout(2);
+// console.log(nm.timeOut.toString());
+}), false);
+
+// Создать область для отображения сообщений
+// Создать макет сообщения. Сообщения должны оборажаться друг на друге без изменения цсс класса

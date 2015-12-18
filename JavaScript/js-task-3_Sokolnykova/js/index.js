84b494da6a1e337ce929a2e88425558b2037f757
xDarynka <sokolnykova@gmail.com>
2015-12-18 15:56:02 +0200

    showNotificationError

--- JavaScript/js-task-3_Sokolnykova/js/index.js
+++ JavaScript/js-task-3_Sokolnykova/js/index.js
@@ -19,7 +19,10 @@
 // nm.showNotification("This is first notification");
 
 
-
+/* теперь нужен метод showNotification, который будет добавлять в эту область дивы с уведомлениями
+и создать кнопку, где будет onclick = nm.showNotification("text here")
+ хотя я бы подписывался так
+$('#btnShowNotification').click(nm.showNotification("textHere")) */
 
 
 
@@ -31,23 +34,44 @@ function NotificationManager(){
 
 
 NotificationManager.prototype.setNotificationTimeout = function(value){
-														  this.timeOut = value;
-														}
+		  this.timeOut = value;
+	}
+
+
 NotificationManager.prototype.createNotificationArea = function(){
-															var nDiv = document.createElement('div');
-													 	  	nDiv.className = "notification";
-													 	  	document.getElementById('na').appendChild(nDiv);
-													 	 	nDiv.innerHTML = text;
+	var nAreaDiv = document.createElement('div');
+	nAreaDiv.className = "notification";
+	nAreaDiv.id = "na";
+	document.getElementsByTagName('body').appendChild(nAreaDiv);
+	nAreaDiv.innerHTML = text;
+}
+
+
+NotificationManager.prototype.showNotificationError = function(text){
+	var errorDiv = document.createElement('div');
+	document.getElementById('na').appendChild(errorDiv);
+	errorDiv.className = "notification-error";
+	errorDiv.innerHTML = text;
+}
+
+
+NotificationManager.prototype.showNotification = function(element, text){
+	
+		// var iv = document.createElement('div');
+		// 
+		
+		
+	
 }
 
 document.addEventListener('DOMContentLoaded', (function(){
 
-													var nm = new NotificationManager();
-													// console.log(nm.timeOut.toString());
-												
-													nm.setNotificationTimeout(2);
-													// console.log(nm.timeOut.toString());
-												}), false);
+	var nm = new NotificationManager();
+	// console.log(nm.timeOut.toString());
+
+	nm.setNotificationTimeout(10000);
+	// console.log(nm.timeOut.toString());
+}), false);
 
 
 

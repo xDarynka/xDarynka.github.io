08d33a488250bb03843daa50d5c07f01787ec52c
xDarynka <sokolnykova@gmail.com>
2015-12-18 13:46:43 +0200

    createNotificationArea() added

--- JavaScript/js-task-3_Sokolnykova/js/index.js
+++ JavaScript/js-task-3_Sokolnykova/js/index.js
@@ -31,18 +31,25 @@ function NotificationManager(){
 
 
 NotificationManager.prototype.setNotificationTimeout = function(value){
- this.timeOut = value;
+														  this.timeOut = value;
+														}
+NotificationManager.prototype.createNotificationArea() = function(){
+															var nDiv = document.createElement('div');
+													 	  	nDiv.className = "notification";
+													 	  	document.getElementById('na').appendChild(nDiv);
+													 	 	nDiv.innerHTML = text;
 }
-
 
 document.addEventListener('DOMContentLoaded', (function(){
 
-var nm = new NotificationManager();
-// console.log(nm.timeOut.toString());
+													var nm = new NotificationManager();
+													// console.log(nm.timeOut.toString());
+												
+													nm.setNotificationTimeout(2);
+													// console.log(nm.timeOut.toString());
+												}), false);
 
-nm.setNotificationTimeout(2);
-// console.log(nm.timeOut.toString());
-}), false);
+
 
 // Создать область для отображения сообщений
 // Создать макет сообщения. Сообщения должны оборажаться друг на друге без изменения цсс класса
